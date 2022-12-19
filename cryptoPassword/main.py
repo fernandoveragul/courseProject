7d6970290ca3bc3e767aef76a871347812a355a0
Fernando Vera <102202733+fernandoveragul@users.noreply.github.com>
2022-12-19 13:44:47 +0500

    Update main.py

--- cryptoPassword/main.py
+++ cryptoPassword/main.py
@@ -1,7 +1,6 @@
 import sys
 import pyperclip
 import design
-from design import resources
 from logic.CP import *
 from PyQt5 import QtWidgets, QtGui
 
@@ -23,17 +22,28 @@ class CurrentApplication(QtWidgets.QMainWindow, design.main_design):
         self.btnCoding.clicked.connect(self.__encoding_decoding)
         self.btnShow.clicked.connect(self.__change_show_mode)
 
-    def __encoding_decoding(self):
-        data: dict = self.__parse_values()
+        # ToolTips:
+        self.btnShow.setToolTip('Кнопка "ПОКАЗАТЬ" выводит сообщение\nс введёнными секретным словом и паролем')
+        self.btnAbout.setToolTip('Кнопка "СПРАВКА" открывает окно справки')
+        self.btnCoding.setToolTip('Кнопка "КОДИРОВАНИЕ" кодирует/декодирует сообщение')
+        self.chbDecoding.setToolTip('Чтобы выполнить декодирование сообщения установите галочку')
+        self.enterSecretWord.setToolTip("Введите секретное слово, правила написания описаны в справке")
+        self.enterSecterPassword.setToolTip("Введите пароль, правила написания описаны в справке")
+
+    def __encoding_decoding(self) -> None:
+        """
+        ``The function encodes/decodes password and secret word, and also handles errors``
+        """
+        data: dict = self.__take_values()
         self.lblOutput.setText('')
         try:
             pyperclip.copy(CryptoPassword.start(**data))
             self.enterSecterPassword.setText('')
             self.enterSecretWord.setText('')
-            self.__message_successful_copy()
+            QtWidgets.QMessageBox.information(self, "Успешно", self.INFO_MESSAGE)
         except CustomExceptions.InvalidHashPassError as ex:
-            QtWidgets.QMessageBox.warning(self, "Ошибка", ex.__doc__)
-        except CustomExceptions.InvalidSomeSymbolError as ex:
+            QtWidgets.QMessageBox.warning(self, "Ошибка", ex.__doc__)  # Error handling without
+        except CustomExceptions.InvalidSomeSymbolError as ex:          # stopping the program cycle
             QtWidgets.QMessageBox.warning(self, "Ошибка", ex.__doc__)
         except CustomExceptions.SmallInputPasswordError as ex:
             QtWidgets.QMessageBox.warning(self, "Ошибка", ex.__doc__)
@@ -42,9 +52,12 @@ class CurrentApplication(QtWidgets.QMainWindow, design.main_design):
         except CustomExceptions.SmallInputSecretWordError as ex:
             QtWidgets.QMessageBox.warning(self, "Ошибка", ex.__doc__)
 
-    def __parse_values(self) -> dict[str: list,
-                                str: str | None,
-                                str: bool]:
+    def __take_values(self) -> dict[str: list, str: str | None, str: bool]:
+        """
+        ``Function takes values from lineEdits``
+
+        :return: Dictionary, for use of syntactic sugar
+        """
         if self.chbDecoding.isChecked():
             sw, sp, is_or = self.enterSecretWord.text(), self.enterSecterPassword.text(), False  # DECODING IS TRUE
             return {'coding': [sw], 'decoding': sp, 'is_origin': is_or}
@@ -52,34 +65,23 @@ class CurrentApplication(QtWidgets.QMainWindow, design.main_design):
             swp, is_or = [self.enterSecretWord.text(), self.enterSecterPassword.text()], True  # DECODING IS FALSE
             return {'coding': swp, 'decoding': None, 'is_origin': is_or}
 
-    def __change_show_mode(self):
+    def __change_show_mode(self) -> None:
         QtWidgets.QMessageBox.information(self, "Пароль и слово",
-                                          f'СС[{self.enterSecretWord.text()[::-1]}]'
-                                          f'\nСП[{self.enterSecterPassword.text()[::-1]}]')
+                                          f'С${self.enterSecretWord.text()[::-1]}'
+                                          f'\nП${self.enterSecterPassword.text()[::-1]}')
 
-    def __message_successful_copy(self):
-        message = QtWidgets.QMessageBox()
-        message.setWindowTitle('Шифровальщик паролей')
-        # message.setIcon(QtWidgets.QMessageBox.Ok)
-        message.setText(self.INFO_MESSAGE)
-        message.exec_()
-
-    def info_about_app(self):
+    def info_about_app(self) -> None:
+        """
+        ``Function make show about window``
+        """
         self.about_window = AboutApplication()
         self.about_window.showMaximized()
 
 
-def main():
+if __name__ == "__main__":
     application = QtWidgets.QApplication(sys.argv)
-    window = CurrentApplication()
     application.setWindowIcon(QtGui.QIcon(":icon.png"))
+    window = CurrentApplication()
+    window.move(window.geometry().center())
     window.show()
     sys.exit(application.exec_())
-
-
-if __name__ == "__main__":
-    main()
-
-    # for MSWindows
-    # cd compile
-    # pyinstaller -w -F -i "..\design\icon.ico" ..\main.py
