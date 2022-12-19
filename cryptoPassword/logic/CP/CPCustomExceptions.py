20a7e4bd8bc34aff3c521f60231bd1dc2d9f803e
Fernando Vera <102202733+fernandoveragul@users.noreply.github.com>
2022-12-19 15:35:21 +0500

    Update CPCustomExceptions.py

--- cryptoPassword/logic/CP/CPCustomExceptions.py
+++ cryptoPassword/logic/CP/CPCustomExceptions.py
@@ -6,11 +6,23 @@ class SmallInputSecretWordError(Exception):
     """ОШИБКА: не выполнено ограничение на длину поля секретного слова, подробнее в справке"""
 
 
-class InvalidSomeSymbolError(Exception):
-    """ОШИБКА: не выполнено ограничение на особые символы, подробнее в справке"""
+class InvalidLowUpCaseSecretWord(Exception):
+    """ОШИБКА: не выполнено ограничение на верхний и нижний регистр символов секретного слова, подробнее в справке"""
 
 
-class InvalidHashPassError(Exception):
+class InvalidLowUpCasePassword(Exception):
+    """ОШИБКА: не выполнено ограничение на верхний и нижний регистр символов секретного слова, подробнее в справке"""
+
+
+class InvalidSpecialSymbolError(Exception):
+    """ОШИБКА: не выполнено ограничение на особые символы в пароле, подробнее в справке"""
+
+
+class InvalidNumberSymbolError(Exception):
+    """ОШИБКА: не выполнено ограничение на цифры в пароле, подробнее в справке"""
+
+
+class InvalidSecretWordsError(Exception):
     """ОШИБКА: секретные слова не совпадают, подробнее в справке"""
 
 
